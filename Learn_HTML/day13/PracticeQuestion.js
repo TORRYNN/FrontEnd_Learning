1b6f9ba4219be3795587f49aad3015933a5b44e5
TORRYNN <prathammchauhann@gmail.com>
2024-08-05 23:32:37 +0530

    Completed JavaScript Part-1

---
+++ Learn_HTML/day13/PracticeQuestion.js
@@ -0,0 +1,16 @@
+let name1="Pratham Chauhan"
+
+console.log(name1.length)
+
+let firstName = "Pratham"
+
+console.log(firstName[0])
+console.log(firstName[(firstName.length)-1])
+
+console.log("apnacollege"+123)
+
+let nam=""
+let nam1=" "
+
+console.log(nam.length)
+console.log(nam1.length)
--- Learn_HTML/day13/day13.js
+++ Learn_HTML/day13/day13.js
@@ -27,3 +27,14 @@ b="pratham"
 console.log(a)
 
 const pi=3.14
+
+// Practice Question
+let age=23
+age+2;
+console.log(age)
+
+let hindi=80
+let eng=90
+let math=100
+let avg=(hindi+eng+math)/3;
+console.log(avg)
